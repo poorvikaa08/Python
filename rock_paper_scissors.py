391c36ba9a6c7b8d636e96b6ac52649a40e89ebf
poorvikaa08 <poorvikaa08@gmail.com>
2024-09-12 12:14:47 +0530

    Rock Paper Scissors

---
+++ rock_paper_scissors.py
@@ -0,0 +1,56 @@
+import random
+
+print("ROCK - PAPER - SCISSORS")
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game_images = [rock, paper, scissors]
+
+player_choice = int(input("What do you choose? Rock, paper or scissors?\nType 0 for rock, 1 for paper and 2 for scissors:  "))
+print(game_images[player_choice])
+
+choices = [0, 1, 2]
+computer_choice = random.choice(choices)
+
+print(f"Computer chose {computer_choice}")
+print(game_images[computer_choice])
+
+if player_choice >=3 or player_choice <0:
+    print("Invalid Number :(")
+if player_choice == 0 and computer_choice == 2:
+    print("Hurray! You WIN.")
+elif player_choice == 2 and computer_choice == 0:
+    print("You LOSE!")    
+elif player_choice < computer_choice:
+    print("You LOSE!")   
+elif player_choice > computer_choice:
+    print("Hurray! You WIN.")   
+elif player_choice == computer_choice:
+    print("It's a draw")
+
+
