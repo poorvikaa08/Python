092617e6ff75d1bbf8c10f240a520104ece3f487
poorvikaa08 <poorvikaa08@gmail.com>
2024-09-22 16:37:14 +0530

    update wordlist

--- wordlist.py
+++ wordlist.py
@@ -1,6 +1,7 @@
 """This file contains list of words """
 
-words = ['abruptly','absurd','abyss','affix','askew','avenue','awkward','axiom','azure','bagpipes','bandwagon','banjo','bayou','beekeeper','bikini','blitz','blizzard','boggle','bookworm','boxcar','boxful','buckaroo','buffalo','buffoon','buxom','buzzard','buzzing','buzzwords','caliph',
+words = ['abruptly','absurd','abyss','affix','askew','avenue','awkward','axiom','azure','bagpipes','bandwagon','banjo','bayou','beekeeper','bikini','blitz',
+    'blizzard','boggle','bookworm','boxcar','boxful','buckaroo','buffalo','buffoon','buxom','buzzard','buzzing','buzzwords','caliph',
     'cobweb','cockiness','croquet','crypt','curacao','cycle','daiquiri','dirndl','disavow','dizzying','duplex','dwarves','embezzle','equip','espionage',
     'euouae','exodus','faking','fishhook','fixable','fjord','flapjack', 'flopping','fluffiness','flyby','foxglove','frazzled','frizzled','fuchsia','funny','gabby',
     'galaxy','galvanize','gazebo','giaour','gizmo','glowworm','glyph', 'gnarly','gnostic','gossip', 'grogginess','haiku', 'haphazard','hyphen','iatrogenic','icebox',
